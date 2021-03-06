fed369424b4360ed096502996bbe539e838420d3
Michail <62438107+Frai-i14@users.noreply.github.com>
2021-03-06 19:31:12 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -478,12 +478,12 @@ function bascketAdd() {
     let closeAll = document.querySelectorAll('.cost__basket_item_close');
     closeAll.forEach(element => {
         element.onclick = event => {
-            alert(event.path[1].children[7].children[0].textContent);
+            alert(event);
             let int = +event.path[1].children[7].children[0].textContent;
                 closeAll = document.querySelectorAll('.cost__basket_item_close');
                 totalValue = totalValue - int;
                 total.textContent = totalValue;
-                event.path[1].remove();
+                document.querySelectorAll('.cost__basket_item')[0].remove();
 
             ///удаленние textarea из верхней формы
             let arrText = document.querySelectorAll('.text_basket_form');
